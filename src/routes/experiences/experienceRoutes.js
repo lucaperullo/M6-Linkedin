891ff88146d39ab101ce1c3d2609a727228a0b6d
GentritBegaj <gentritbegaj@gmail.com>
2021-05-10 13:22:41 +0200

    1st crud

--- src/routes/experiences/experienceRoutes.js
+++ src/routes/experiences/experienceRoutes.js
@@ -1,2 +1,21 @@
 import express from "express";
-import asyncHandler from "../../core/asyncHandler";
+import asyncHandler from "../../core/asyncHandler.js";
+import ExperienceModel from "../../database/mongo/models/ExperienceModel.js";
+
+const router = express.Router();
+
+router.get(
+  "/:userId/reviews",
+  asyncHandler(async (req, res, next) => {
+    const experiences = await ExperienceModel.find();
+    res.send(reviews);
+  })
+);
+
+router.get(
+  "/:userId/reviews/:reviewId",
+  asyncHandler(async (req, res, next) => {
+    const experience = await ExperienceModel.findById(req.params.reviewId);
+    res.send(experience);
+  })
+);
