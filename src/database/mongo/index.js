53782d904c695c96c65a61f9f58a5a2a47c5e2cc
luca <lucaperullo@outlook.it>
2021-05-10 20:31:52 +0200

    colored console messages

--- src/database/mongo/index.js
+++ src/database/mongo/index.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
-import Logger from '../../core/loggerHandler.js';
-import {mongoURI, mongoDB} from '../../config.js';
+import mongoose from "mongoose";
+import Logger from "../../core/loggerHandler.js";
+import { mongoURI, mongoDB } from "../../config.js";
 
 const options = {
   useNewUrlParser: true,
@@ -15,40 +15,49 @@ const options = {
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
 };
 
-const mongoURL = `mongodb+srv://${mongoDB.mdbUser}:${mongoDB.mdbPassword}@${mongoDB.mdbHost}/${mongoDB.mdbDatabase}?retryWrites=true&w=majority`
+const mongoURL = `mongodb+srv://${mongoDB.mdbUser}:${mongoDB.mdbPassword}@${mongoDB.mdbHost}/${mongoDB.mdbDatabase}?retryWrites=true&w=majority`;
 
 // Create the database connection
 // Exported to server.js file and is called into action when server starts
 export const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(mongoURI, options)
-    Logger.info(`Mongo ☁ Cloud Atlas | ${mongoDB.mdbHost} | User: ${mongoDB.mdbUser} | Connection Established ✅`)
+    await mongoose.connect(mongoURI, options);
+    console.log(
+      "\u001b[" +
+        32 +
+        "m" +
+        `Mongo ☁ Cloud Atlas | ${mongoDB.mdbHost} | User: ${mongoDB.mdbUser} | Connection Established ✅` +
+        "\u001b[0m"
+    );
 
     // When successfully connected
-    mongoose.connection.on('connected', () => {
+    mongoose.connection.on("connected", () => {
       Logger.info(`Mongoose default open to ${mongoURL}`);
     });
 
     // If the connection throws an error
-    mongoose.connection.on('error', (err) => {
-      Logger.error('Mongoose default connection error: ' + err);
+    mongoose.connection.on("error", (err) => {
+      Logger.error("Mongoose default connection error: " + err);
     });
 
     // When the connection is disconnected
-    mongoose.connection.on('disconnected', () => {
-      Logger.info('Mongoose default connection disconnected');
+    mongoose.connection.on("disconnected", () => {
+      Logger.info("Mongoose default connection disconnected");
     });
 
     // If the Node process ends, close the Mongoose connection
-    process.on('SIGINT', () => {
+    process.on("SIGINT", () => {
       mongoose.connection.close(() => {
-        Logger.info('Mongoose default connection disconnected through app termination');
+        Logger.info(
+          "Mongoose default connection disconnected through app termination"
+        );
         process.exit(0);
       });
     });
-
   } catch (e) {
-    Logger.info('Mongoose connection error, if this is a fresh project maybe you forgot to set the env variables');
+    Logger.info(
+      "Mongoose connection error, if this is a fresh project maybe you forgot to set the env variables"
+    );
     Logger.error(e);
   }
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import {port} from "./config.js";
+import { port } from "./config.js";
 import Logger from "./core/loggerHandler.js";
-import app from './app.js'
-import {connectToMongoDB} from "./database/mongo/index.js"
+import app from "./app.js";
+import { connectToMongoDB } from "./database/mongo/index.js";
 
-app.listen(port, async() => {
-  Logger.info(`Server running on port : ${port}`)
-  await connectToMongoDB()
-}).on('error', (e) => Logger.error(e))
+app
+  .listen(port, async () => {
+    console.log(
+      "\u001b[" + 35 + "m" + `Server running on port : ${port}` + "\u001b[0m"
+    );
+    await connectToMongoDB();
+  })
+  .on("error", (e) => Logger.error(e));
