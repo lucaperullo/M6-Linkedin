179753c8078111fecaac2074fec8b4a7609a8346
luca <lucaperullo@outlook.it>
2021-05-10 12:02:18 +0200

    user model in progress

--- src/database/mongo/models/UserModel.js
+++ src/database/mongo/models/UserModel.js
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 import ExperienceModel from "./ExperienceModel";
 
-
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: "A name is required",
+  },
+  surname: {
+    type: String,
+    required: "A surname is required",
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  bio: {},
+  experiences: [ExperienceModel],
+});
 
-  name: String,
-  experiences: [ExperienceModel]
-
-})
-
-export default model('user', UserSchema)
+export default model("user", UserSchema);
