4e0d458fadd7ddc5723850b45e5402919161a17f
GentritBegaj <gentritbegaj@gmail.com>
2021-05-10 17:36:57 +0200

    Cruds finished

--- src/routes/v1/profile/userRoutes.js
+++ src/routes/v1/profile/userRoutes.js
@@ -2,6 +2,11 @@ import express from "express";
 import { asyncHandler } from "../../../core/asyncHandler.js";
 import { BadRequestError, NotFoundError } from "../../../core/apiErrors.js";
 import UserModel from "../../../database/mongo/models/UserModel.js";
+import ExperienceModel from "../../../database/mongo/models/ExperienceModel.js";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { extname } from "path";
 
 const router = express.Router();
 
@@ -38,6 +43,7 @@ router.post(
   })
 );
 
+
 router.put(
   "/:id",
   asyncHandler(async (req, res, next) => {
@@ -57,8 +63,148 @@ router.delete(
     const user = await UserModel.findByIdAndDelete(req.params.id);
     if (user) {
       res.send({ message: "user destroyed" }).status(204);
+router.get(
+  "/:userId/experiences",
+  asyncHandler(async (req, res, next) => {
+    const { experiences } = await UserModel.findById(req.params.userId);
+    res.send(experiences);
+  })
+);
+
+router.get(
+  "/:userId/experiences/:experienceId",
+  asyncHandler(async (req, res, next) => {
+    const experience = await UserModel.findById(
+      {
+        _id: req.params.userId,
+      },
+      {
+        experiences: {
+          $elemMatch: { _id: req.params.experienceId },
+        },
+      }
+    );
+    res.send(experience);
+  })
+);
+
+router.post(
+  "/:userId/experiences",
+  asyncHandler(async (req, res, next) => {
+    const newExperience = new ExperienceModel(req.body);
+    const experience = { ...newExperience.toObject() };
+    await UserModel.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $push: {
+          experiences: { ...experience, created_At: new Date() },
+        },
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.send(experience).status(201);
+  })
+);
+
+router.put(
+  "/:userId/experiences/:experienceId",
+  asyncHandler(async (req, res, next) => {
+    const modifiedExperience = await UserModel.findOneAndUpdate(
+      {
+        _id: req.params.userId,
+        "experiences._id": req.params.experienceId,
+      },
+      {
+        $set: {
+          "experiences.$": {
+            ...req.body,
+            _id: req.params.experienceId,
+            updated_At: new Date(),
+          },
+        },
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (!modifiedExperience) {
+      new NotFoundError(`User not found!`);
+    }
+    res.status(202).send(modifiedExperience);
+  })
+);
+
+router.delete(
+  "/:userId/experiences/:experienceId",
+  asyncHandler(async (req, res, next) => {
+    const modifiedUser = await UserModel.findByIdAndUpdate(
+      {
+        _id: req.params.userId,
+      },
+      {
+        $pull: {
+          experiences: { _id: req.params.experienceId },
+        },
+      },
+      { new: true }
+    );
+    if (modifiedUser) {
+      res.send("User experiences modified");
+    } else {
+      const error = new Error();
+      error.httpStatusCode = 404;
+      next(error);
     }
   })
 );
 
+const cloudStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "LinkedIn",
+  },
+});
+
+const cloudMulter = multer({
+  storage: cloudStorage,
+  fileFilter: function (req, file, next) {
+    const acceptedExtensions = [".png", ".jpg", ".gif", "bmp", ".jpeg"];
+    if (!acceptedExtensions.includes(extname(file.originalname))) {
+      return next(
+        new ErrorResponse(
+          `Image type not allowed: ${extname(file.originalname)}`
+        )
+      );
+    }
+    next(null, true);
+  },
+});
+
+router.post(
+  "/:userId/upload/:experienceId",
+  asyncHandler(async (req, res, next) => {
+    const modifiedUser = await UserModel.findByIdAndUpdate(
+      {
+        _id: req.params.userId,
+        "experiences._id": req.params.experienceId,
+      },
+      {
+        $set: {
+          "experiences.$": {
+            ...req.body,
+            image: req.file.path,
+            _id: req.params.experienceId,
+            updatedAt: new Date(),
+          },
+        },
+      },
+      { new: true }
+    );
+    res.send(modifiedUser);
+  })
+);
 export default router;
